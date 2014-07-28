8791ac50015ec37accf7b195120b075874ed55f7
Ben Stokoe and Richard Dowden <skyhaikuteam+benstokoe+daffydowden@gmail.com>
2014-07-28 13:52:55 +0100

    Remove pry. Whoops.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -4,7 +4,6 @@ describe Aokay::Helpers, type: :feature do
     it "should return the last sitecat request" do
       visit '/'
       make_ajax_req "http://metrics.sky.com"
-      binding.pry
       result = last_sitecat_request
       expect(result.host).to eql "metrics.sky.com"
     end
