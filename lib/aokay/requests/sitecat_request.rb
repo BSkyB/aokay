59d5c1c08ef664e74e627f836277e9c77d1b36c2
Richard Dowden <skyhaikuteam+daffydowden@gmail.com>
2015-01-27 14:35:32 +0000

    Add start of minitest adaptor

--- lib/aokay/requests/sitecat_request.rb
+++ lib/aokay/requests/sitecat_request.rb
@@ -16,5 +16,21 @@ module Aokay
     def pageload?
       tracked :event and (tracked(:event).include? 'event1')
     end
+
+    def events
+      @parsed_url.query_values['events'].split(',')
+    end
+
+    def tracked key, type=:eVar
+      if field_ref[key].is_a? Hash
+        @parsed_url.query_values[field_ref[key][type]]
+      else
+        super
+      end
+    end
+
+    private #----------------------------------------------
+
+
   end
 end
--- spec/sitecat_request_spec.rb
+++ spec/sitecat_request_spec.rb
@@ -9,4 +9,8 @@ describe Aokay::SitecatRequest, "#find_requests", type: :feature do
     expect(result.last).to be_a_kind_of(Aokay::SitecatRequest)
   end
 
+  it "should allow lookup of variables with friendly names"
+    visit '/'
+    make_ajax_req "http://tracking.example.com"
+    result = Aokay::SitecatRequest.all
 end
